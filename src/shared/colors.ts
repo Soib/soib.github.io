882d11ea345ce44ea70f6ad1521dcfd2bcd4d518
Łukasz Cybulski <lukasz_cybulski@icloud.com>
2021-08-07 16:55:59 +0200

    Updates

--- src/shared/colors.ts
+++
@@ -1,7 +0,0 @@
-export const neons = {
-  pink: "#f000ff",
-  yellow: "#ffe700",
-  green: "#74ee15",
-  cyan: "#4deeea",
-  blue: "#001eff",
-};
--- src/shared/links.ts
+++
@@ -1,5 +0,0 @@
-export const socials = {
-  github: "https://github.com/soib",
-  mail: "mailto:lukaszcybulski@pm.me",
-  linkedin: "https://linkedin.com/in/lukaszcybulski",
-};
