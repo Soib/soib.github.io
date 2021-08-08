4f47d7c0d70fe2c61b1d9e8624d0d404a2be43fc
Łukasz Cybulski <lukasz_cybulski@icloud.com>
2021-08-08 11:15:32 +0200

    Fix warnings

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -2,17 +2,13 @@ import AnimatedLogo from "../../components/AnimatedLogo/AnimatedLogo";
 import "./HomePage.style.scss";
 import Navbar from "../../components/Navbar/Navbar";
 import FindMeOn from "../../components/FindMeOn/FindMeOn";
-import StarBackground from "../../components/StarBackground/StarBackground";
 
 const HomePage = () => {
   return (
     <div className="container">
-      {/* <StarBackground /> */}
-      {/* <div className="content-container"> */}
       <Navbar />
       <AnimatedLogo />
       <FindMeOn />
-      {/* </div> */}
     </div>
   );
 };
